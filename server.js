20315187dcb7bb27af6040cf6281bf4e789dc7b2
David Delgado <djd9062@gmail.com>
2017-06-22 12:49:48 -0500

    reinstalled eslint

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 
-
 mongoose.connect(process.env.MONGO_URI, (err) => {
-    if (err) { console.error(err) }
-    else {
-      app.listen(PORT, () => {
-        console.log(`Listening at ${PORT}`);
-      });    
-        console.log('db connected');
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    app.listen(PORT, () => {
+      console.log(`Listening at ${PORT}`);
+    });
+    console.log('db connected');
+  }
 });
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+  res.send('Hello World!');
+});
