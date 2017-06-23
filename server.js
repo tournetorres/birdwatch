6251790900b2618732b7ea2b2af796be85d5016a
Daniel Baker <djbaker1.007@gmail.com>
2017-06-22 19:19:35 -0500

    [feat] db helper

--- server.js
+++ server.js
@@ -3,19 +3,16 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
+const db = require('./lib/psql');
 
 
-mongoose.connect(process.env.MONGO_URI, (err) => {
-    if (err) { console.error(err) }
-    else {
-      app.listen(PORT, () => {
-        console.log(`Listening at ${PORT}`);
-      });    
-        console.log('db connected');
-    }
-});
 
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT}`);
+})
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
+
+
