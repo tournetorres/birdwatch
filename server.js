79c661e328f3932f2bde8b0535e5d92ba8ba3dfd
Daniel Baker <djbaker1.007@gmail.com>
2017-06-26 11:51:58 -0500

    added extra packages for auth, requiring files in server.js

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const birdCall = require('./lib/birdApi');
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const strategy = require('passport-local').Strategy;
 
 const PORT = process.env.PORT;
 const app = express();
 const db = require('./lib/psql');
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+  secret: 'it\'s a secret man',
+  resave: false,
+  saveUninitiated: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static('public'))
 
 const birdCatcher = (data) => {
   console.log(JSON.parse(data));
 };
 
 const obj = { lat: 40, lng: -90 };
-
-birdCall.call(obj, birdCatcher);
