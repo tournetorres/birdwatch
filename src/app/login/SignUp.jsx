3011b6281bd9599b74a0f0a218dc0ffd740b5cf2
Daniel Baker <djbaker1.007@gmail.com>
2017-06-26 19:25:10 -0500

    login/signup page now posts with axios to server

--- src/app/login/SignUp.jsx
+++ src/app/login/SignUp.jsx
@@ -3,6 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import axios from 'axios';
 
 const style = {
   margin: 15,
@@ -15,6 +16,35 @@ class SignUp extends Component {
       username: '',
       password: '',
     };
+  }
+  handleClick(event) {
+    var apiBaseUrl = 'http://localhost:3000/';
+    var self = this;
+    var payload={
+      "email":this.state.username,
+      "password":this.state.password
+    };
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+      console.log(response);
+    if(response.status == 200){
+      console.log("Login successfull");
+    var uploadScreen=[];
+    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+    }
+    else if(response.status == 204){
+      console.log("Username password do not match");
+      alert("username password do not match")
+    }
+    else{
+      console.log("Username does not exists");
+      alert("Username does not exist");
+    }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
   render() {
     return (
