a431b73b3af2cba20a29331c3e21d4ed6d2eb5f8
Rose Espiritu <rose.espiritu1@gmail.com>
2017-07-01 18:45:34 -0500

    [feat] image loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
-// transpile our scss into a css folder for out public directory
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-// uglify our client facing files
 const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
-
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPluginConfig
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './public'),
     filename: 'bundle.js',
+  },
+  resolve: { // These options change how modules are resolved
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css', '.jpeg', '.jpg', '.gif', '.png'], // Automatically resolve certain extensions
+    alias: { // Create aliases
+      images: path.resolve(__dirname, 'src/assets/images')  // src/assets/images alias
+    }
   },
   module: {
     rules: [
@@ -21,22 +26,46 @@ const config = {
         loader: 'babel-loader',  //babel-core
       },
       {
-        test: /\.jsx$/, // all files ending with .jsx
-        exclude: /node_modules/, // exclude searching for files in the node_modules directory
-        loader: 'babel-loader', // use the babel-loader for all .jsx files
-      },
-      {
         test: /\.scss$/, // files ending with.scss
         use: ExtractTextWebpackPlugin.extract({ // extract method
           use: ['css-loader', 'sass-loader'],
           fallback: 'style-loader',  //fallback for any CSS not extracted
         }),
       },
+      {
+        test: /\.jsx$/, // all files ending with .jsx
+        exclude: /node_modules/, // exclude searching for files in the node_modules directory
+        loader: 'babel-loader', // use the babel-loader for all .jsx files
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]', {  // images loader
+          loader: 'image-webpack-loader',
+          query: {
+            mozjpeg: {
+              progressive: true,
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            optipng: {
+              optimizationLevel: 4,
+            },
+            pngquant: {
+              quality: '75-90',
+              speed: 3,
+            },
+          },
+        }],
+        exclude: /node_modules/,
+        include: __dirname,
+      },
     ],
   },
   plugins: [
     new ExtractTextWebpackPlugin('styles.css'), // call and name our css file
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
+    // new HtmlWebpackPlugin(),
   ],
   devServer: {
     contentBase: path.resolve(__dirname, './public'), // url to serve HTML contents from
