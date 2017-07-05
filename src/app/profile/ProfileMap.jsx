02148f2a5c8b629ae91d652023951a312cfa08e8
Daniel Weill <daniel.zalmen@gmail.com>
2017-07-05 15:24:30 -0500

    (update) added profile map file

---
+++ src/app/profile/ProfileMap.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Map, { Marker, InfoWindow } from 'google-maps-react';
+
+class ProfileMap extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Honey Im Comb</div>
+    );
+  }
+}
+export default ProfileMap;
