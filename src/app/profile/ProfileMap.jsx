f1e9c34b36ef2854b9faf917ab2ce5066b85a944
Daniel Weill <daniel.zalmen@gmail.com>
2017-07-06 22:55:15 -0500

    (feat) add geocodes to profile map

--- src/app/profile/ProfileMap.jsx
+++ src/app/profile/ProfileMap.jsx
@@ -32,27 +32,16 @@ class ProfileMap extends Component {
       google={this.props.google}
       initialCenter={{ lat: 29.95106579999999, lng: -90.0715323 }}
       >
-      {this.state.coords.map(coord => {
+      {this.state.coords.map((coord, i) => {
         console.log(coord ,'i am the message for you');
-      })}
-      {/*{this.props.profileData.map((bird, i, arr) => {
-        console.log(bird);
-        geocodeByAddress(bird.location)
-        .then(results => getLatLng(results[0]))
-        .then((latLng) => {
-          this.lat = latLng.lat;
-          console.log(this.lat, 'im big and lat');
-          this.lng = latLng.lng;
-          console.log(this.lng, 'im big and lng')
-        });
-          return (
+        return (
             <Marker
               key={i}
               icon={BirdLogo}
-              position={{ lat: this.lat, lng: this.lng }}
+              position={{ lat: coord.lat, lng: coord.lng }}
             />
           );
-      })}*/}
+      })}
       </Map>
       </div>
     );
